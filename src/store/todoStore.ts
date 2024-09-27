ce0ef5eefb894d942ebe2a33840ec638d0ffcb59
jduran <jduran@gen-it.com.ar>
2024-09-26 22:02:36 -0300

    add remove todo function

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -4,9 +4,11 @@ import { Todo } from "../interfaces/todo.interface";
 interface TodoStore {
   todos: Todo[];
   addTodo: (todo: Todo) => void;
+  removeTodo: (todo: Todo) => void;
 }
 
 export const useTodoStore = create<TodoStore>((set) => ({
-  todos: [{ id: 1, text: 'Buy milk' }],
+  todos: [],
   addTodo: (todo) => set((state) => ({ todos: [...state.todos, todo] })),
+  removeTodo: (todo) => set((state) => ({ todos: state.todos.filter(t => t.id !== todo.id) })),
 }))
