ba3cea4910e6f816a3b5e9925af36049927fde90
jduran <jduran@gen-it.com.ar>
2024-09-26 22:13:16 -0300

    styles for items

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -10,25 +10,9 @@ export default function Item({ todo }: Props) {
   const { removeTodo } = useTodoStore()
 
   return (
-    <div key={todo.id} style={{
-      border: '1px solid #fff',
-      borderRadius: '5px',
-      marginBottom: '20px',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: '10px',
-    }}>
+    <div key={todo.id} className="item">
       <span>{todo.text}</span>
-      <button
-        onClick={() => removeTodo(todo)}
-        style={{
-          padding: '10px',
-          borderRadius: '5px',
-          backgroundColor: 'red',
-          marginLeft: '10px',
-        }}
-      >Remover</button>
+      <button onClick={() => removeTodo(todo)} className="cleaner">Remover</button>
     </div>
 
   )
